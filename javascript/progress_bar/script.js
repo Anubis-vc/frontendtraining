e5c3a201af5cb932806ddaeaf713e2fe18b2492f
Anubis-vc <114633659+Anubis-vc@users.noreply.github.com>
2024-11-22 12:52:31 -0500

    added scripting

---
+++ javascript/progress_bar/script.js
@@ -0,0 +1,11 @@
+const num = document.getElementById("number");
+let counter = 0;
+setInterval(() => {
+	if (counter == 65) {
+		clearInterval();
+	}
+	else {
+		counter += 1;
+		num.innerHTML = counter + "%";
+	}
+}, 30);

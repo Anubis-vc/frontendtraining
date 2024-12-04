a8daf037a2726f1a9d593bfd17a7f5f0b4173c9b
Anubis-vc <114633659+Anubis-vc@users.noreply.github.com>
2024-12-03 21:47:34 -0500

    react components

---
+++ react/my-first-react-app/src/Greeting.jsx
@@ -0,0 +1,8 @@
+export const Greeting = () => {
+	return <h1>&quot;I drink your milkshake, Eli!&quot;</h1>
+	;
+}
+
+export const Casablanca = () => {
+	return <h2>&quot;I think this is the start of a beautiful friendship&quot;</h2>
+}

bf94bedf6198e16f51388b6c0467c3806e24e9d2
Anubis-vc <114633659+Anubis-vc@users.noreply.github.com>
2024-12-14 18:18:29 -0500

    practicing with states and hooks

---
+++ react/states/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Person() {
+  const [person, setPerson] = useState({ firstName: "", lastName: "", age: 100 });
+
+  const handleIncreaseAge = () => {
+    setPerson({ ...person, age: person.age + 1 });
+  };
+
+  const handleFirstNameChange = e => {
+    setPerson( {firstName: e.target.value, lastName: person.lastName, age: person.age});
+  }
+  const handleLastNameChange = e => {
+    setPerson( {firstName: person.firstName, lastName: e.target.value, age: person.age});
+  }
+
+  return (
+    <>
+      <h1>{person.firstName + person.lastName}</h1>
+      <h2>{person.age}</h2>
+      <input type="text" placeholder="enter first name" onChange={handleFirstNameChange}/>
+      <input type="text" placeholder='enter last name' onChange={handleLastNameChange}/>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+    </>
+  );
+}
+
+
+function App() {
+  return (
+    <>
+      <Person />
+    </>
+  )
+}
+
+export default App

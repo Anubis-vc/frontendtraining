2b6511502b169a550be4680e4ec1d3f52781e812
Anubis-vc <114633659+Anubis-vc@users.noreply.github.com>
2025-01-14 09:28:21 -0500

    added footer

--- react/shopping_site/src/App.jsx
+++ react/shopping_site/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer/Footer';
 import Home from './Routes/HomePage/Home';
 import About from './Routes/About/About';
 import Contact from './Routes/Contact/Contact';
@@ -16,6 +17,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 };
---
+++ react/shopping_site/src/Components/Footer/Footer.jsx
@@ -0,0 +1,53 @@
+import styles from "./footer.module.css";
+
+import instagram from "../../assets/Socials/instagram.svg";
+import linkedin from "../../assets/Socials/linkedin.svg";
+import twitter from "../../assets/Socials/twitter.svg";
+// import letterboxd from "../../assets/Socials/letterboxd-mono.svg";
+import github from "../../assets/Socials/github.svg";
+
+function Footer () {
+	const socials = [
+		{svg: instagram, href: "https://www.instagram.com/ved.chugh", label: "Instagram"},
+		{svg: linkedin, href: "https://www.linkedin.com/in/vedchugh/", label: "LinkedIn"},
+		{svg: twitter, href: "https://x.com/notved", label: "Twitter"},
+		{svg: github, href: "https://github.com/Anubis-vc", label: "Github"},
+		// {svg: letterboxd, href: "https://t.co/lzV089lSvM", label: "Letterboxd"},
+	];
+
+	const links = [
+		{text: "Legal", href: "#"},
+		{text: "Privacy Policy", href: "#"},
+		{text: "Security", href: "#"},
+		{text: "Manage Cookies", href: "#"},
+	];
+
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.container}>
+
+				<div className={styles.socialIcons}>
+					{socials.map(link => (
+						<a
+							key={link.label}
+							href={link.href}
+							className={styles.socialLink}
+							aria-label={link.label}
+						>
+							<img src={link.svg} alt={link.label} />
+						</a>
+					))}
+				</div>
+
+				<div className={styles.logoSection}>
+					<span className={styles.logo}>Made by Ved Chugh</span>
+					<div className={styles.copyright}>
+						Find at <a href="https://github.com/Anubis-vc" target="_blank">my Github</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;

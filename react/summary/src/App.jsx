83cb61cab82190c1f1b27eec65d275fade4c0bc5
Anubis-vc <114633659+Anubis-vc@users.noreply.github.com>
2025-01-30 19:12:05 -0500

    beginning supplement site for portfolio

---
+++ react/summary/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className='h-screen w-screen bg-inherit'>
+      <Header />
+      
+      {/* Scrollable carousel */}
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ react/summary/src/Components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { SocialIcon } from 'react-social-icons'
+function Header() {
+  return (
+	<header className='flex items-center justify-around top-0 
+	h-16 md:h-20 z-20 p-5 border-b-2 shadow-[0px_6px_0px_#000]'>
+		<div className='space-x-5'>
+			<SocialIcon
+				target='_blank'
+				url="https://www.twitter.com/notved"
+			/>
+			<SocialIcon
+				target='_blank'
+				url="https://www.linkedin.com/in/vedchugh/"
+			/>
+		</div>
+		<h1 className='text-4xl'>
+			Ved's Frontend
+		</h1>
+		<div className='space-x-5'>
+			<SocialIcon
+				target='_blank'
+				url="https://github.com/Anubis-vc"
+			/>
+			<SocialIcon
+				target='_blank'
+				url="https://letterboxd.com/vedchugh/"
+			/>
+		</div>
+	</header>
+  )
+}
+
+export default Header
